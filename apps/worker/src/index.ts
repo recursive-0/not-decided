d51c9557a70e5505572f729ab1e7ca5881ad894c
Recursive <attriai@Attris-MacBook-Air.local>
2025-05-04 09:07:41 +0530

    Add all the allowed origins to make the cors origin dynamic

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -2,35 +2,59 @@ import { Env } from '../worker-configuration';
 import { generateStream } from './routes/generate-stream';
 import { initializeStream } from './routes/init-stream';
 
+const ALLOWED_ORIGINS = ['https://wrisor-dev.pages.dev', 'http://localhost:5173'];
+
 function cors(handler: (req: Request, env: Env, ctx: ExecutionContext) => Promise<Response>) {
-    return async (req: Request, env: Env, ctx: ExecutionContext) => {
+	return async (req: Request, env: Env, ctx: ExecutionContext) => {
+		const requestOrigin = req.headers.get('Origin');
+		let responseOrigin = '';
 
-        const corsHeadersConfig: HeadersInit = {
-            'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
-            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-            'Access-Control-Allow-Origin': "*", 
-        };
+		if (requestOrigin && ALLOWED_ORIGINS.includes(requestOrigin)) {
+			responseOrigin = requestOrigin;
+		} else {
+			console.log(`Origin ${requestOrigin} not allowed by CORS policy.`);
+		}
 
-        if (req.method === 'OPTIONS') {
-            return new Response(null, {
-                status: 204,
-                headers: corsHeadersConfig,
-            });
-        }
+		const corsHeaders: HeadersInit = {
+			'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+			'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+		};
 
-        const response = await handler(req, env, ctx);
+		if (responseOrigin) {
+			corsHeaders['Access-Control-Allow-Origin'] = responseOrigin;
+		}
 
-        const finalHeaders = new Headers(response.headers); // This properly copies original headers
+		if (req.method === 'OPTIONS') {
+			if (responseOrigin) {
+				return new Response(null, {
+					status: 204,
+					headers: corsHeaders,
+				});
+			} else {
+				return new Response('CORS policy does not allow this origin for OPTIONS', { status: 403 });
+			}
+		}
 
-        finalHeaders.set('Access-Control-Allow-Origin', corsHeadersConfig['Access-Control-Allow-Origin']);
+		const response = await handler(req, env, ctx);
 
+		const finalHeaders = new Headers(response.headers);
 
-        return new Response(response.body, {
-            status: response.status,
-            statusText: response.statusText,
-            headers: finalHeaders, 
-        });
-    };
+		if (responseOrigin) {
+			finalHeaders.set('Access-Control-Allow-Origin', responseOrigin);
+		} else {
+			finalHeaders.delete('Access-Control-Allow-Origin');
+		}
+
+		finalHeaders.set('Access-Control-Allow-Headers', corsHeaders['Access-Control-Allow-Headers']!);
+
+		finalHeaders.append('Vary', 'Origin');
+
+		return new Response(response.body, {
+			status: response.status,
+			statusText: response.statusText,
+			headers: finalHeaders,
+		});
+	};
 }
 
 const initializeStreamWithCors = cors(initializeStream);
